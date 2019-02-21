d1ae1d99d451c588463b68a8bfbc35b6526fe79d
hg2128yd <hg2128yd@W3110-S1750835.century.edu>
2019-02-21 13:48:19 -0600

    first

---
+++ src/Calculater.java
@@ -0,0 +1,24 @@
+
+public class Calculater {
+	
+	public double add(double num1, double num2) {
+		double sum = num1 + num2;
+		return sum;
+	}
+	public double subtract(double num1, double num2) {
+		double ans = num1 - num2;
+		return ans;
+	}
+	public double multiple(double num1, double num2) {
+		double ans = num1 * num2;
+		return ans;
+	}
+	public double divide(double num1, double num2) {
+		double ans = num1 / num2;
+		return ans;
+	}
+	public double reminder(double num1, double num2) {
+		double ans = num1 % num2;
+		return ans;
+	}
+}
